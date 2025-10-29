9d814f8b0b1f25cb0218ea2bb42b151bdd0058a9
marvieiradev <marcosvieira482@gmail.com>
2025-10-29 18:58:23 -0300

    feat: initial API settings added

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,38 @@
 import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifyCors } from "@fastify/cors";
+import ScalarApiReference from "@scalar/fastify-api-reference";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Webhook Inspector API",
+      description: "API documentation for the Webhook Inspector service",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+});
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("Server is running");
